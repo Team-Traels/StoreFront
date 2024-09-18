0ad93459da6d1fac775d3decf56abac48e1deebf
JustDondo <162633910+CodeDondo@users.noreply.github.com>
2024-09-18 11:36:44 +0200

    Mulig ændring?
    Klar til dataReceive?

--- assets/js/localstorage.js
+++ assets/js/localstorage.js
@@ -34,6 +34,32 @@ function saveBasketItems(items) {
     localStorage.setItem(this.basketItemsKey, JSON.stringify(items));
 }
 
+
+    // Hent alle produkter fra DummyJSON API med fejlhåndtering
+   function fetchAllProducts(dataReceive) {
+        fetch(this.apiUrl)
+            .then(response => {
+                if (!response.ok) {
+                    // Hvis HTTP status ikke er OK, kast en fejl
+                    throw new Error(`HTTP error! Status: ${response.status}`);
+                }
+                return response.json(); // Returner JSON data hvis status er OK
+            })
+            .then(data => {
+                dataReceive(data.products); // Send produktlisten til dataReceive for klargøring til display
+            })
+            .catch(error => {
+                console.error('Error fetching products:', error); // Log fejl
+            });
+    }
+
+    function dataReceive(data) {
+        console.log('hej', data);
+    }
+    
+    fetchAllProducts();
+
+
 window.onload = function() {
     getBasketItems();
     saveBasketItems();
