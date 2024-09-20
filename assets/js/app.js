ccb0e46cfb53dbda75c33fbdf2b2d2bb507715bb
FrederikWagnerC <frederikwagnercs@gmail.com>
2024-09-20 08:51:04 +0200

    logo On Click

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,3 +5,13 @@ function saveLocalData() {
     let saveableData = JSON.stringify(basket)
     localStorage.setItem('data', saveableData)
 }
+
+const logo = document.getElementById('logo')
+let mainPageLoaded = true
+
+logo.addEventListener('click', (e) => {
+    if (!mainPageLoaded) {
+        buildMainPage()
+        mainPageLoaded = true;
+    }
+})
