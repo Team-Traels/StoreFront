283872f092b0cb907cff0e725ef78a81ac32799e
FrederikWagnerC <frederikwagnercs@gmail.com>
2024-09-26 09:24:21 +0200

    buildMainPage() #20

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,50 @@
 let basket = []
+let products = []
+
+// FETCH PRODUCTS
+function getProducts() {
+    // Fetcher products
+    fetch('https://dummyjson.com/products')
+        .then(function (response) {
+            // Tjekker om responsen er ok
+            if (!response.ok) {
+                // Hvis ikke, giver en fejl
+                throw new Error('Error: Fejlede I at hente produkter');
+            }
+            // Konveterer responsen til json
+            return response.json();
+        })
+        .then(function (data) {
+            // Tjekker om resonsen er I det rigtige format
+            if (data && Array.isArray(data.products)) {
+                // Hvis data er korrekt, giv det vidre til receivedProducts
+                receivedProducts(data.products);
+                products = data.products
+              
+
+            } else {
+                // Hvis dataformatet er forkert, giv en error!
+                throw new Error('Error: Forkert data format');
+            }
+        })
+        // Alle errors bliver catched her
+        .catch(function (error) {
+            console.error(error.message);
+            errorData();
+        });
+}
+
+function errorData() {
+    console.log('Nej, vi har ikke hvad du skal bruge!');
+}
+function receivedProducts(products) {
+    
+    if (!firstTimePageLoaded) {
+        buildMainPage(products)
+        firstTimePageLoaded = true;
+        
+    }
+}
 
 
 function saveLocalData() {
@@ -8,9 +54,11 @@ function saveLocalData() {
 
 const logoDocument = document.getElementById('logo')
 const basketDocument = document.getElementById('basket')
+const mainContent = document.getElementById('content')
 
 let mainPageLoaded = true
 let basketPageLoaded = false
+let firstTimePageLoaded = false
 
 logoDocument.addEventListener('click', (e) => {
     if (!mainPageLoaded) {
@@ -20,15 +68,36 @@ logoDocument.addEventListener('click', (e) => {
     }
 })
 
-basketDocument.addEventListener('click', (e) => {
-    if (!basketPageLoaded) {
-        buildMainPage()
-        basketPageLoaded = true;
-        mainPageLoaded = false;
-    }
-})
+// basketDocument.addEventListener('click', (e) => {
+//     if (!basketPageLoaded) {
+//         buildMainPage()
+//         basketPageLoaded = true;
+//         mainPageLoaded = false;
+//     }
+// })
 
 
 function productCallback(productID) {
-     bygProductPage(productID)
+    bygProductPage(productID)
 }
+
+function buildMainPage(products) {
+    let randomNumber = Math.round(Math.random() * products.length)
+    let featuredProduct = products[randomNumber]
+    mainContent.innerHTML = ''
+    let featuredProductHTML = `
+    <div class="featured-product">
+        <img src="${featuredProduct.thumbnail}" alt="${featuredProduct.title}">
+        <h2>${featuredProduct.title}</h2>
+        <p>${featuredProduct.price}$</p>
+        <button onclick="productCallback(${featuredProduct.id})">Læs mere</button>`
+    mainContent.innerHTML = featuredProductHTML
+    console.log(products);
+    
+}
+
+
+// Køre funktionen når siden loader
+window.addEventListener('load', (e) => {
+    getProducts();
+});
